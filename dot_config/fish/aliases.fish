468d4e8a4d82eca81d8528bb5e75c38fc89e087e
Adam Bisinella <adam.bisinella@icloud.com>
2025-03-14 11:06:36 +0000

    Added Itim font

--- dot_config/fish/aliases.fish
+++ dot_config/fish/aliases.fish
@@ -2,8 +2,7 @@
 alias 'clears' 'printf "\ec\e[3J"'
 
 # Neovim
-alias vi nvim
-alias vif 'nvim "$(fzf)"'
+alias nvif 'nvim "$(fzf)"'
 
 # Eza 
 alias ls='eza --color=always --icons=auto --group-directories-first'
@@ -11,6 +10,3 @@ alias ll='ls --long'
 alias la='ls --all'
 alias tree='eza --tree --git-ignore --color=always --icons=auto'
 alias tree4='tree --level=4'
-
-# Chezmoi
-alias cm chezmoi
