080f15a0033307e16a0c038a3e6b2b7466bb9ff8
Adam Bisinella <adam.bisinella@icloud.com>
2025-03-20 21:50:53 +0000

    Added requirements.yml and sdkman.yml for ansible bootstrapping

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # Install ansible
-sudo pacman -Sy ansible
+sudo pacman -Sy --needed ansible
+echo "Ansible installation complete."
 
-# Install ansible module for AUR packages
-ansible-galaxy collection install kewlfft.aur
+# Install galaxy modules 
+ansible-galaxy install -r ~/.bootstrap/requirements.yml
+echo "Ansible collections installation complete."
 
 # Run ansible installation
 ansible-playbook -i ~/.bootstrap/inventory.yml ~/.bootstrap/setup.yml --ask-become-pass
-
-echo "Ansible installation complete."
+echo "Ansible playbook setup.yml competed."
