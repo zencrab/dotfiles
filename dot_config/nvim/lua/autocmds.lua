fd76a7dcbfa45ce4c8f2b8325ef0ce74c6370e10
Adam Bisinella <adam.bisinella@icloud.com>
2025-04-08 18:46:07 +0100

    Telescope border temporary fix for Neovim 0.11

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -1,14 +1,14 @@
-vim.api.nvim_create_autocmd("User", {
-  pattern = "BlinkCmpMenuOpen",
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("User", {
+  pattern = "TelescopeFindPre",
   callback = function()
-    vim.b.copilot_suggestion_hidden = true
+    vim.opt_local.winborder = "none"
+    vim.api.nvim_create_autocmd("WinLeave", {
+      once = true,
+      callback = function()
+        vim.opt_local.winborder = "rounded"
+      end,
+    })
   end,
 })
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "BlinkCmpMenuClose",
-  callback = function()
-    vim.b.copilot_suggestion_hidden = false
-  end,
-})
-
