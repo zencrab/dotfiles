0c7abc602843a1b596d17511e5650249aa1176e5
zencrab <adam.bisinella@icloud.com>
2025-05-14 15:15:20 +0100

    Yazi modular nix configuration

--- dot_config/home-manager/yazi.nix
+++ dot_config/home-manager/yazi.nix
@@ -8,4 +8,20 @@
             full-border = pkgs.yaziPlugins.full-border;
         };
     };
+
+    # Desktop entry
+    xdg.desktopEntries."yazi" = {
+        name = "Yazi";
+        genericName = "Terminal Emulator";
+        comment = "A terminal emulator for X11 and Wayland";
+
+        exec = "kitty yazi";
+        terminal = false;
+
+        categories = [
+            "Application"
+            "System"
+            "FileManager"
+        ];
+    };
 }
