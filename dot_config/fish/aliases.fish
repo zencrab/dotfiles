57ac43ef64795406a2017b3c841ea0993793874e
Adam Bisinella <adam.bisinella@icloud.com>
2025-05-23 17:53:24 +0100

    [fish] better aliases

--- dot_config/fish/aliases.fish
+++ dot_config/fish/aliases.fish
@@ -1,31 +1,25 @@
-# Clear the screen and scrollback history
+# clear the screen
 alias clears 'printf "\ec\e[3J"'
 
-# Neovim
-alias fz 'nvim "$(fzf)"'
+# power management
+alias off 'systemctl poweroff'
+alias reb 'systemctl reboot'
 
-# Eza 
-if type -q eza
-  alias ls 'eza --color=always --icons=auto --group-directories-first'
-  alias ll 'ls --long'
-  alias la 'ls --all'
-  alias tree 'eza --tree --git-ignore --color=always --icons=auto'
-  alias tree4 'tree --level=4'
-end
+# eza 
+alias ls 'eza --color=always --icons=auto --group-directories-first'
+alias ll 'ls --long'
+alias la 'ls --all'
+alias tree 'eza --tree --git-ignore --color=always --icons=auto'
+alias tree4 'tree --level=4'
 
-# Bat
-if type -q bat
-  alias cat 'bat --paging=never'
-end
-
-# Bat
+# bat
 alias cat 'bat --paging=never'
 
 # gomi
-if type -q gomi
-  alias rm 'gomi'
-end
+alias rm 'gomi'
 
-if type -q fastfetch
-  alias fastani 'gum spin --spinner minidot --title "Rendering system logo and fetching info..." -- kitten icat -n --place 30x30@0x3 --scale-up --align left ~/.logos/nixos/animation-60fps.gif | fastfetch --logo-width 30 --raw -'
-end
+# chezmoi
+alias cm 'chezmoi'
+
+# fastfetch
+alias fastani 'gum spin --spinner minidot --title "Rendering system logo and fetching info..." -- kitten icat -n --place 30x30@0x3 --scale-up --align left ~/.logos/nixos/animation-60fps.gif | fastfetch --logo-width 30 --raw -'
